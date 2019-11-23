f385732f6b1d50639b088d34df9732b6cf42a674
Samantha Gaiser <sgaiser3@gmail.com>
2019-11-23 10:29:44 -0500

    initial api call being added

--- memer.js
+++ memer.js
@@ -1,1 +1,20 @@
-// help
+let = searchTerm = $('userEntry').val();
+const queryUrl = "https://api.imgur.com/3/gallery/search?q="+ searchTerm
+// "Authorization: Client-ID {{32366db57d27d55}}" to go in html header?
+
+$('#GetMyMeme').on('click',function(){
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+    })
+    .then(function(response){
+        var results = response.data;
+            if (results === movieName){
+            // creating a div for our mem result
+            var memeDiv = $('<resultdiv>');
+            // creating image tag
+            var memeImg = $('<img>');
+        }
+
+    });
+});  
