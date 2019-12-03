2a41c7f4228f3d9853b631915ec78f73b80b06ef
Taylor Zettel <tzettel@gmail.com>
2019-12-02 19:17:10 -0500

    linked gasp cdn and taylorGSAP.js at bottom of body, added bar-anim class to header elements, added res-dis class to results divs, animated both to enter screen from the right and stop at correct position

---
+++ taylorGSAP.js
@@ -0,0 +1,6 @@
+gsap.from('.bar-anim', {opacity: 0, duration: 1, x: 200, stagger: 0.6});
+
+$('#add-meme').on('click', function(event) {
+  event.preventDefault();
+  gsap.from('.res-dis', {opacity: 0, duration: 1, x: 500, stagger: 0.6});
+})
